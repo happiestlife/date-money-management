8045cd8bc11ce2889015b790267eabbd2c2ea6ab
happiestlife <chickenman10@naver.com>
2022-02-15 20:21:11 +0900

    login function add

--- src/main/java/project1/dateMoneyManagement/controller/BaseController.java
+++ src/main/java/project1/dateMoneyManagement/controller/BaseController.java
@@ -2,9 +2,11 @@ package project1.dateMoneyManagement.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import project1.dateMoneyManagement.Member;
 import project1.dateMoneyManagement.repository.member.MemberRepository;
 
@@ -24,8 +26,11 @@ public class BaseController {
     }
 
     @GetMapping
-    public String gotoLogin() {
-        log.info("loginPage");
+    public String gotoLogin(RedirectAttributes redirect) {
+        log.info("goto loginPage");
+
+        redirect.addAttribute("logFailed", false);
+
         return "redirect:/login";
     }
 
@@ -44,13 +49,4 @@ public class BaseController {
                 "ruri", null, "현성2", "보민2"));
 
     }
-    private String id;
-    private String password;
-    private String email;
-    private Date regDate;
-    private String nickname;
-    private BufferedImage image;
-    private String boyName;
-    private String girlName;
-
 }
