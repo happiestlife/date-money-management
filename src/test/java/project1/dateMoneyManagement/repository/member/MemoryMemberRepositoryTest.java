74e1b90e43da33d669efc3bc4dfd4fab373e6be6
happiestlife <chickenman10@naver.com>
2022-02-25 23:33:00 +0900

    update : 로그인 서비스 테스트 코드 추가

--- src/test/java/project1/dateMoneyManagement/repository/member/MemoryMemberRepositoryTest.java
+++ src/test/java/project1/dateMoneyManagement/repository/member/MemoryMemberRepositoryTest.java
@@ -1,27 +1,34 @@
 package project1.dateMoneyManagement.repository.member;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import project1.dateMoneyManagement.Member;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
 
     private MemoryMemberRepository memberRepository = new MemoryMemberRepository();
     private List<String> idList = new ArrayList<>();
 
+    @BeforeEach
+    void insertSample() {
+        Member member1 = new Member("chickenman10", "1234", "chickenman10@naver.com",
+                "chickenman", null, "현성", "보민");
+        Member member2 = new Member("ruri", "0820", "aaaa@naver.com",
+                "ruri", null, "현성2", "보민2");
+
+        memberRepository.insert(member1);
+        memberRepository.insert(member2);
+
+        idList.add(member1.getId());
+        idList.add(member2.getId());
+    }
 
     @Test
-    /**
-     * 이미지 없이 회원정보 저장
-     */
     void insertDataWithNoImage() {
         Member member = new Member("test", "1234", "test@naver.com",
                 "test", null, "현성", "보민");
@@ -37,11 +44,10 @@ public class MemoryMemberRepositoryTest {
     // 이미지와 함께 회원정보 저장은 아직 만들지 않았다.
 
     @Test
-    // 업데이트 성공
     void updateMember_Success() {
         String id = idList.get(0);
         Member updateMember = new Member(id, "2222", "test@gmail.com",
-                                "test", "현성test", "보민test");
+                                "test", null,"현성test", "보민test");
 
         memberRepository.update(id, updateMember);
 
@@ -57,14 +63,12 @@ public class MemoryMemberRepositoryTest {
     }
 
     @Test
-    // 업데이트 실패( id가 존재 X )
-    void updateMember_Fail() {
+    void updateMember_FailByWrongId() {
         String id = "xxxx";
         Member updateMember = new Member(id, "2222", "test@gmail.com",
                 "test", "현성test", "보민test");
 
-        assertThrows(NoSuchElementException.class,
-                ()-> memberRepository.update(id, updateMember));
+        assertThat(memberRepository.update(id, updateMember)).isEqualTo(null);
     }
 
     @Test
@@ -79,11 +83,10 @@ public class MemoryMemberRepositoryTest {
     }
 
     @Test
-    void removeMember_Fail() {
+    void removeMember_FailByWrongId() {
         String id = "xxxxx";
 
-        assertThrows(NoSuchElementException.class,
-                ()->memberRepository.remove(id));
+        assertThat(memberRepository.remove(id)).isFalse();
     }
 
     @Test
@@ -98,33 +101,18 @@ public class MemoryMemberRepositoryTest {
     }
 
     @Test
-    void findById_Fail() {
+    void findById_FailByWrongId() {
         Member findMember = memberRepository.findById("xxxxx");
         assertThat(findMember).isEqualTo(null);
     }
 
     @Test
-    void findAllMembers() {
+    void findAllMembers_Success() {
         List<Member> members = memberRepository.findAll();
         assertThat(members.size()).isEqualTo(2);
 
         String id = idList.get(0);
         Member findMember = memberRepository.findById(id);
         assertThat(members).contains(findMember);
-    }
-
-
-    @BeforeEach // 각 테스트 메서드 실행 전에 실행됨
-    void insertSample() {
-        Member member1 = new Member("chickenman10", "1234", "chickenman10@naver.com",
-                "chickenman", null, "현성", "보민");
-        Member member2 = new Member("ruri", "0820", "aaaa@naver.com",
-                "ruri", null, "현성2", "보민2");
-
-        memberRepository.insert(member1);
-        memberRepository.insert(member2);
-
-        idList.add(member1.getId());
-        idList.add(member2.getId());
     }
 }
--- src/test/java/project1/dateMoneyManagement/service/login/LoginServiceTest.java
+++ src/test/java/project1/dateMoneyManagement/service/login/LoginServiceTest.java
@@ -1,9 +1,155 @@
 package project1.dateMoneyManagement.service.login;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import project1.dateMoneyManagement.Member;
+import project1.dateMoneyManagement.exception.login.DuplicateIdException;
+import project1.dateMoneyManagement.exception.login.NoEnoughInfoException;
+import project1.dateMoneyManagement.exception.login.WrongIdOrPasswordException;
+import project1.dateMoneyManagement.exception.login.WrongMatchException;
+import project1.dateMoneyManagement.repository.member.MemoryMemberRepository;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginServiceTest {
 
-    @Test
+    private LoginService loginService = new LoginServiceImpl(new MemoryMemberRepository(), new AuthMailServiceTest());
+    private Member member = new Member("test", "1234", "test@naver.com",
+                                "test", null, "test", "test");
 
+    @BeforeEach
+    public void insertData() {
+        loginService.register(member);
+    }
+
+    @Test
+    public void register_Success(){
+        Member member = new Member("test1", "1111", "test",
+                "test", null, "test", "test");
+
+        loginService.register(member);
+
+        Member findMember = loginService.findMemberById(member.getId());
+
+        assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    public void register_FailBySameId(){
+        Member member1 = new Member("test1", "1", "test1",
+                "test1", null, "test1", "test1");
+        Member member2 = new Member("test1", "2222", "test2",
+                "test2", null, "test2", "test2");
+
+        loginService.register(member1);
+
+        assertThrows(DuplicateIdException.class, () -> loginService.register(member2));
+    }
+
+    @Test
+    public void register_FailByNotEnoughData(){
+        Member noId = new Member("", "1", "test",
+                "test", null, "test", "test");
+        Member noPw = new Member("test1", "", "test",
+                "test", null, "test", "test");
+        Member noEmail = new Member("test2", "1", "",
+                "test", null, "test", "test");
+        Member noNickname = new Member("test3", "1", "test",
+                "", null, "test", "test");
+        Member noBoyName= new Member("test4", "1", "test",
+                "test", null, "", "test");
+        Member noGirlName = new Member("test5", "1", "test",
+                "test", null, "test", "");
+
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noId));
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noPw));
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noEmail));
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noNickname));
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noBoyName));
+        assertThrows(NoEnoughInfoException.class, () -> loginService.register(noNickname));
+    }
+
+    @Test
+    public void login_Success() {
+        Member findMember = loginService.login(member.getId(), member.getPassword());
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    public void login_FailByWrongMatch() {
+        assertThrows(WrongIdOrPasswordException.class, ()->loginService.login(member.getId(), "wrongPw"));
+        assertThrows(WrongIdOrPasswordException.class, ()->loginService.login("wrongId", member.getPassword()));
+        assertThrows(WrongIdOrPasswordException.class, ()->loginService.login("wrongId", "wrongPw"));
+    }
+
+    @Test
+    public void findMemberById_Success() {
+        Member findMember = loginService.findMemberById(member.getId());
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    public void findMemberById_FailByWrongId() {
+        assertThrows(NoSuchElementException.class, () -> loginService.findMemberById("tttt"));
+    }
+
+    @Test
+    public void findIdByEmail_Success() {
+        String findId = loginService.findIdByEmail(member.getEmail());
+
+        assertThat(member.getId()).isEqualTo(findId);
+    }
+
+    @Test
+    public void findIdByEmail_FailByNoMatchEmail() {
+        assertThrows(NoSuchElementException.class, () -> loginService.findIdByEmail("wrongEmail"));
+    }
+
+    @Test
+    public void sendAuthCode_FailByWrongData() {
+        assertThrows(NoSuchElementException.class, () -> loginService.sendAuthCode("wrongId", member.getEmail()));
+        assertThrows(NoSuchElementException.class, () -> loginService.sendAuthCode(member.getId(), "wrongEmail"));
+        assertThrows(NoSuchElementException.class, () -> loginService.sendAuthCode("wrongId", "wrongEmail"));
+    }
+
+    @Test
+    public void verifyCode_Success() {
+        String code = loginService.sendAuthCode(member.getId(), member.getEmail());
+
+        boolean flag = loginService.verifyCode(member.getId(), code);
+
+        assertThat(flag).isTrue();
+    }
+
+    @Test
+    public void verifyCode_FailByNoMatchId() {
+        String code = loginService.sendAuthCode(member.getId(), member.getEmail());
+
+        assertThrows(WrongMatchException.class, () -> loginService.verifyCode("wrongId", code));
+    }
+
+    @Test
+    public void verifyCode_FailByWrongMatchCode() {
+        String code = loginService.sendAuthCode(member.getId(), member.getEmail());
+
+        assertThrows(WrongMatchException.class, () -> loginService.verifyCode(member.getId(), "wrongCode"));
+    }
+
+    @Test
+    public void updatePw_Success() {
+        boolean flag = loginService.updatePw(member.getId(), "newPassword", "newPassword");
+
+        assertThat(flag).isTrue();
+    }
+
+    @Test
+    public void updatePw_FailByWrongMatchPasswordAndCheck() {
+        assertThrows(WrongMatchException.class, () -> loginService.updatePw(member.getId(), "newPassword", "wrongPassword"));
+    }
 }
